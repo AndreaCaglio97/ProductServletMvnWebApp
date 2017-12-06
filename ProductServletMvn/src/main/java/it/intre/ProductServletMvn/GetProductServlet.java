f379ea29139a2d5a91cef1062631fe3f7e6b76c6
Caglio Andrea <caglio@intre.it>
2017-12-06 10:24:20 +0100

    refactor delle servlet

--- ProductServletMvn/src/main/java/it/intre/ProductServletMvn/GetProductServlet.java
+++ ProductServletMvn/src/main/java/it/intre/ProductServletMvn/GetProductServlet.java
@@ -10,29 +10,32 @@ import static it.intre.ProductServletMvn.database.DBManager.*;
 public class GetProductServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         System.out.println("Inside doPost");
+        String[] attributes = getAttributes(request);
+        printAttributes(attributes);
+        printHtmlResponse(response, attributes);
+    }
 
+    private static String[] getAttributes(HttpServletRequest request) {
         String[] attributes = new String[6];
-
         attributes[0] = request.getParameter("id_product");
         attributes[1] = request.getParameter("name");
         attributes[2] = request.getParameter("is_imported");
         attributes[3] = request.getParameter("minimum_price");
         attributes[4] = request.getParameter("maximum_price");
         attributes[5] = request.getParameter("category");
+        return attributes;
+    }
 
+    private static void printAttributes(String[] attributes) {
         for(String c : attributes) {
             System.out.println(c);
         }
+    }
 
-        // get response writer
+    private void printHtmlResponse(HttpServletResponse response, String[] attributes) throws IOException {
         PrintWriter writer = response.getWriter();
-
-        // build HTML code
         String htmlResponse = productList(attributes);
-
-        // return response
         writer.println(htmlResponse);
     }
 }
--- ProductServletMvn/src/main/java/it/intre/ProductServletMvn/InsertProductServlet.java
+++ ProductServletMvn/src/main/java/it/intre/ProductServletMvn/InsertProductServlet.java
@@ -14,23 +14,27 @@ import static it.intre.ProductServletMvn.database.DBManager.*;
 public class InsertProductServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         System.out.println("Inside doPost");
+        Product product = getFormFields(request);
+        insertSingleProductToDB(product);
+        printHtmlResponse(response, product);
+    }
 
-        // read form fields
+    private static Product getFormFields(HttpServletRequest request) {
         String name = request.getParameter("name");
         boolean isImported = Boolean.parseBoolean(request.getParameter("is_imported"));
         double price = Double.parseDouble(request.getParameter("price"));
         Category category = Category.valueOf(request.getParameter("category"));
-        Product product = new Product(name,isImported,price,category,1);
+        return new Product(name,isImported,price,category,1);
+    }
 
-        insertSingleProductToDB(product);
-
-
-        // get response writer
+    private static void printHtmlResponse(HttpServletResponse response, Product product) throws IOException {
         PrintWriter writer = response.getWriter();
+        String htmlResponse = htmlResponseMaker(product);
+        writer.println(htmlResponse);
+    }
 
-        // build HTML code
+    private static String htmlResponseMaker(Product product) {
         String htmlResponse = "<html>" +
                 "<head>" +
                 "   <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css\"> " +
@@ -46,18 +50,15 @@ public class InsertProductServlet extends HttpServlet {
                 "    <th>Category</th>\n" +
                 "</tr>";
         htmlResponse += "<tr>";
-        htmlResponse += "<td>" + name + "</td>";
-        htmlResponse += "<td>" + isImported + "</td>";
-        htmlResponse += "<td>" + price + "</td>";
-        htmlResponse += "<td>" + category + "</td>";
+        htmlResponse += "<td>" + product.getName() + "</td>";
+        htmlResponse += "<td>" + product.isImported() + "</td>";
+        htmlResponse += "<td>" + product.getPrice() + "</td>";
+        htmlResponse += "<td>" + product.getCategory() + "</td>";
         htmlResponse += "</tr>";
         htmlResponse += "</table>" +
                 "<script src='http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js'></script>" +
                 "</body>" +
                 "</html>";
-
-        // return response
-        writer.println(htmlResponse);
-
+        return htmlResponse;
     }
 }
